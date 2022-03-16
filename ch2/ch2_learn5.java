e9509708a5a0f5b1899a979a5e58f26316edb28d
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-17 07:21:18 +0900

    Update ch2_learn5.java

--- ch2/ch2_learn5.java
+++ ch2/ch2_learn5.java
@@ -1,8 +1,8 @@
 public class ch2_learn5 {
     public static void main(String[] args){
-    	System.out.println(10.); // 10.0
-        	System.out.println(.10); // 0.10
-        	System.out.println(10f); // 10.0
-        	System.out.println(1e3); // 1000.0
+        System.out.println(10.); // 10.0
+        System.out.println(.10); // 0.10
+        System.out.println(10f); // 10.0
+        System.out.println(1e3); // 1000.0
     }
 }
