dea39d21197c1df3003009e1ae1b41f58776d5b7
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-19 07:04:27 +0900

    Add files via upload
    ch3를 업로드 했습니다.

---
+++ ch3/ch3_learn1.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args){
+       	int i = 'A' // int 타입이 char보다 크기때문에 문자코드 65가 출력됩니다.
+        long L = 123; // long 타입이 int보다 크기 때문에 저장이 가능합니다.
+        double d =3.14f // double 타입이 float보다 크기 때문에 저장이 가능합니다.
+    }
+}
---
+++ ch3/ch3_learn4.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args){
+    	char ch1 = 'A'; // 정상으로 동작합니다.
+        char ch2 = 'AB'; // 문자열이기 때문에 에러가 납니다.
+        char ch3 = ''; //에러가 납니다.
+    }
+}
