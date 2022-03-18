5e4aeffef0f1b35c836cda579f52bd2576f8651a
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-19 07:04:43 +0900

    Update ch3_learn1.java

--- ch3/ch3_learn1.java
+++ ch3/ch3_learn1.java
@@ -1,4 +1,4 @@
-public class test {
+public class ch3_learn1 {
     public static void main(String[] args){
        	int i = 'A' // int 타입이 char보다 크기때문에 문자코드 65가 출력됩니다.
         long L = 123; // long 타입이 int보다 크기 때문에 저장이 가능합니다.
