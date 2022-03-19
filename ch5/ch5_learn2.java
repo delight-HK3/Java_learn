863a46b764f6b7303f399a82977c95781c3a8ccc
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-20 08:56:06 +0900

    Update ch5_learn2.java

--- ch5/ch5_learn2.java
+++ ch5/ch5_learn2.java
@@ -1,4 +1,4 @@
-public class ch_temp_learn2 {
+public class ch5_learn2 {
     public static void main(String[] args) {
         // 8진수와 16진수의 접두사도 같이 출력하려면 지시자앞에 접두사를 붙여서 출력시키면 됩니다.
         System.out.printf("%#o%n", 20); // 024
