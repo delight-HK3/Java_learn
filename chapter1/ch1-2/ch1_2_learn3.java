aecb88c59fab8886314497b530c0c178f377c4df
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-21 10:13:24 +0900

    Update ch1_2_learn3.java

--- chapter1/ch1-2/ch1_2_learn3.java
+++ chapter1/ch1-2/ch1_2_learn3.java
@@ -1,7 +1,7 @@
 public class ch1_2_learn3 {
     public static void main(String[] args){
     	int num = 100; // 10진수
-        	int oct = 010; // 8진수, 10진수로 8
+        int oct = 010; // 8진수, 10진수로 8
        	int hex = 0x10; // 16진수, 10진수로 16
     }
 }
