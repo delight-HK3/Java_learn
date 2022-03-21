dd66963835c049e8f3d9f2d1d4c16017aeeab328
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-21 10:09:38 +0900

    Delete ch5 directory

--- ch5/ch5_learn1.java
+++
@@ -1,9 +0,0 @@
-public class ch5_learn1 {
-    public static void main(String[] args) {
-        //같은 값을 넣어도 지시자가 다르면 결과가 이렇게 나옵니다.
-        System.out.printf("%d%n", 20); // 20 10진수 dec
-        System.out.printf("%o%n", 20); // 24 8진수 oct
-        System.out.printf("%x%n", 20); // 14 16진수 hex
-        System.out.printf("%s%n", Integer.toBinaryString(20)); // 10100 2진수
-    }
-}
