d79f5a1679a0722c1fe9eeadebafaf216082b50e
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-21 10:09:07 +0900

    Delete ch1 directory

--- ch1/ch1_learn1.java
+++
@@ -1,5 +0,0 @@
-public class ch1_learn1 {
-    public static void main(String[] args){
-        int x; //변수의 선언
-    }
-}
--- ch1/ch1_learn5.java
+++
@@ -1,7 +0,0 @@
-public class ch1_learn5 {
-    public static void main(String[] args){
-        final int con_score = 100; 
-        // 상수를 지정하려면 final을 붙이면 됩니다.
-        System.out.println(con_score);
-    }
-}
