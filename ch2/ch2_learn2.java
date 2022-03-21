fd53ee4e2f3fe1157003121fae4bc0d1caafc057
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-21 10:09:16 +0900

    Delete ch2 directory

--- ch2/ch2_learn2.java
+++
@@ -1,8 +0,0 @@
-public class ch2_learn2 {
-    public static void main(String[] args){
-        boolean torf = false;
-        char ch = 'A';
-        string str = "ABC";
-        byte b = 127; // byte 와 short의 리터럴은 없기 때문에 int타입입니다.
-    }
-}
--- ch2/ch2_learn3.java
+++
@@ -1,7 +0,0 @@
-public class ch2_learn3 {
-    public static void main(String[] args){
-    	int num = 100; // 10진수
-        int oct = 010; // 8진수, 10진수로 8
-       	int hex = 0x10; // 16진수, 10진수로 16
-    }
-}
