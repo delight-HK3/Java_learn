9e3fb87e54d47d65bd0d691cfeeb6998a6397845
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-21 10:09:23 +0900

    Delete ch3 directory

--- ch3/ch3_learn3.java
+++
@@ -1,6 +0,0 @@
-public class ch3_learn3 {
-    public static void main(String[] args){
-    	byte b1 = 100; // byte의 범위인 (-128 ~ 127)에 포함 되기 때문에 저장이 가능합니다.
-        byte b2 = 128; // byte의 범위를 넘겼기 때문에 에러가 납니다.
-    }
-}
