f7df27a773c6d6cfd0066713ea084661835de246
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-22 09:44:09 +0900

    Update and rename ch3_learn2.java to ch1_3_learn2.java

--- chapter1/ch1-3/ch1_3_learn2.java
+++ chapter1/ch1-3/ch1_3_learn2.java
@@ -1,4 +1,4 @@
-public class test {
+public class ch1_3_learn2 {
     public static void main(String[] args){
        	int i = 30_000_0000; // int 타입이 저장 가능한 범위를 벗어났기 때문에 에러가 납니다.
         long L = 3.14f; // 기본적으로 실수형은 정수형보다 범위가 넓기 때문에 에러가 납니다.
