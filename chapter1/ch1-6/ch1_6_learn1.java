b3c00480c4478cd0c25a134b0469e80c93da6dee
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-23 09:49:25 +0900

    Update and rename ch6_learn1.java to ch1_6_learn1.java

--- chapter1/ch1-6/ch1_6_learn1.java
+++ chapter1/ch1-6/ch1_6_learn1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ch6_learn1 {
+public class ch1_6_learn1 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in); // scan 객체 선언
 
