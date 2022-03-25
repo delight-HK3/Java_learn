1399393dd112e95ba423875f30d2514a811ad8a0
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-25 13:29:28 +0900

    Add files via upload

---
+++ ch2-2/ch2_2_learn2.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        int su1;
+        su1 = 10 + 20 - 5 + 1;
+        System.out.println(su1); // 26
+        // 먼저 10, 20을 더하고 5를 뺀 뒤 마지막으로 1을 더한다.
+
+        int x, y;
+        x = y = 10;
+        System.out.printf("%d %d%n",x,y);
+        //10을 y에 저장하고 10이 있는 y를 x에 저장하여 x,y는 10이 된다.
+    }
+}
