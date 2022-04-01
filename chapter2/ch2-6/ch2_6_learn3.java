0889ca15408c4a720ea4b576a8d308487b731ac8
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-01 09:03:57 +0900

    Add files via upload
    ch2-6을 추가했습니다.

---
+++ chapter2/ch2-6/ch2_6_learn3.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args) {
+        String str1 = "test";
+        String str2 = "test";
+        String str3 = new String("testtest");
+        String str4 = new String("testtest");
+
+        System.out.println(str1 == str2); // true
+        System.out.println(str1.equals(str2)); // true
+
+        System.out.println(str3 == str4); // false
+        System.out.println(str3.equals(str4)); // true
+    }
+}
