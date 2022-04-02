8975ab8993df6c3d1d37e63fbea092cadcf78dd2
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-02 09:02:53 +0900

    Add files via upload
    ch2-8을 추가했습니다.

---
+++ chapter2/ch2-8/ch2_8_learn1.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        int su1 = 10 > 5 ? 10 : 20;
+        // 비교 연산에서 결과값이 true이면 ? 오른쪽의 수 중 왼쪽에 있는 것을 저장하고
+        // false면 20을 저장한다.
+        System.out.println(su1);
+    }
+}
