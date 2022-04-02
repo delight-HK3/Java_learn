6a23498073dc2dcb8fdc6d900c0a538efebbabbd
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-02 09:03:23 +0900

    Update ch2_8_learn1.java

--- chapter2/ch2-8/ch2_8_learn1.java
+++ chapter2/ch2-8/ch2_8_learn1.java
@@ -1,4 +1,4 @@
-public class test {
+public class ch2_8_learn1 {
     public static void main(String[] args) {
         int su1 = 10 > 5 ? 10 : 20;
         // 비교 연산에서 결과값이 true이면 ? 오른쪽의 수 중 왼쪽에 있는 것을 저장하고
