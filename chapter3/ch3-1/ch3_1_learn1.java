4d003b8b80c4d5662b02f3a53c88f5f991eaa0de
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-03 08:16:57 +0900

    Add files via upload
    chapter3를 추가했습니다.

---
+++ chapter3/ch3-1/ch3_1_learn1.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        int num = 15;
+        if(10 <= num && num <= 20){
+            System.out.printf("%d는 10 ~ 20 사이에 있는 수입니다.",num);
+        }
+        else{
+            System.out.printf("%d는 10 ~ 20 사이에 없는 수입니다.",num);
+        }
+    }
+}
