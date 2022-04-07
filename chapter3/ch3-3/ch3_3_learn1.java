48f510bc70162aa1609fb38d91981c30aaf4daf1
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-07 09:40:50 +0900

    Add files via upload
    ch3-3을 추가했습니다.

---
+++ chapter3/ch3-3/ch3_3_learn1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("정수를 입력하세요 : ");
+        int num = scan.nextInt();
+        switch (num){ //num 변수와 case 입력값과 같은 case를 실행
+            case 1:{ // num에 1이 입력된 경우
+                System.out.println("1 입력");
+                break;
+            }
+            case 2:{ // num에 2이 입력된 경우
+                System.out.println("2 입력");
+                break;
+            }
+            case 3:{ // num에 3이 입력된 경우
+                System.out.println("3 입력");
+                break;
+            }
+            default:{ // num에 1 ~ 3 이외의 값이 입력된 경우 
+                System.out.println("1 ~ 3 이외의 수 입력");
+            }
+        }
+    }
+}
