71c6fecf9cdb2352f5151208308fc9dfe3867310
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-07 09:41:15 +0900

    Update ch3_3_learn2.java

--- chapter3/ch3-3/ch3_3_learn2.java
+++ chapter3/ch3-3/ch3_3_learn2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class test {
+public class ch3_3_learn2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.printf("문자열을 입력하세요 : ");
