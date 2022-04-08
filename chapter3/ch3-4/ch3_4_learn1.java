b0b15083b7b82252ca7214424a3e49751c6c1b73
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-09 08:50:58 +0900

    Update ch3_4_learn1.java

--- chapter3/ch3-4/ch3_4_learn1.java
+++ chapter3/ch3-4/ch3_4_learn1.java
@@ -1,4 +1,4 @@
-public class test {
+public class ch3_4_learn1 {
     public static void main(String[] args) {
         System.out.printf("%f%n",Math.random());
         // 0.1 ~ 0.9 사이 double형 난수를 출력
