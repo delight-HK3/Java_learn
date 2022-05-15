cb709551e3591bdff3730977adc8cac03189f474
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-05-15 09:17:41 +0900

    Delete chapter3 directory

--- chapter3/ch3-5/ch3_5_learn3.java
+++
@@ -1,8 +0,0 @@
-public class ch3_5_learn3 {
-    public static void main(String[] args) {
-        for(int i = 0; i < 5; i--){
-            // 조건이 계속 참이기 때문에 무한으로 루프를 돌린다.
-            System.out.println(i);
-        }
-    }
-}
