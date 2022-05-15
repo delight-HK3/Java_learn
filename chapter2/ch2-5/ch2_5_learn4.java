1b3b63af302d24f4d8f2256159a3795990849fca
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-05-15 09:17:28 +0900

    Delete chapter2 directory

--- chapter2/ch2-5/ch2_5_learn4.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        int sum1 = 10;
-        int sum2 = 3;
-
-        System.out.println(sum1 % sum2); // 나머지값인 1
-        System.out.println(sum1 % -sum2); // 부호는 무시
-    }
-}
